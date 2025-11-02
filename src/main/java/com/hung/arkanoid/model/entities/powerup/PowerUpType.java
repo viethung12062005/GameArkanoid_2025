9f4783b920bae7bbf87a791d42dac2c415ffd29a
viethung <viethung12062005@gmail.com>
2025-11-02 13:10:48 +0700

    Thêm PowerUp enum và class PowerUpFactory, chưa implement các PowerUp

---
+++ src/main/java/com/hung/arkanoid/model/entities/powerup/PowerUpType.java
@@ -0,0 +1,15 @@
+package com.hung.arkanoid.model.entities.powerup;
+
+public enum PowerUpType {
+    EXPAND,
+    SHRINK,
+    LASER,
+    MULTI_BALL,
+    CATCH,
+    SLOW_BALL,
+    FAST_BALL,
+    FIRE_BALL,
+    BARRIER,
+    EXTRA_LIFE
+}
+
