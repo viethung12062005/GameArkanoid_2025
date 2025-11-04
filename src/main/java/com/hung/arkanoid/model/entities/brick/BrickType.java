f6a56d2336422cc77e3fba6fd9b1e7b611b2e69f
viethung <viethung12062005@gmail.com>
2025-11-04 08:06:31 +0700

    Thêm một số phương thức cho Brick và thêm BrickType

--- src/main/java/com/hung/arkanoid/model/entities/brick/BrickType.java
+++ src/main/java/com/hung/arkanoid/model/entities/brick/BrickType.java
@@ -21,30 +21,9 @@ package com.hung.arkanoid.model.entities.brick;
  */
 
 public enum BrickType {
-    NORMAL(60, 20, 1),
-    STRONG(60, 20, 3),
-    WIDE(120, 20, 2);
-
-    private final double width;
-    private final double height;
-    private final int hitPoints;
-
-    BrickType(double width, double height, int hitPoints) {
-        this.width = width;
-        this.height = height;
-        this.hitPoints = hitPoints;
-    }
-
-    public double getWidth() { return width; }
-    public double getHeight() { return height; }
-    public int getHitPoints() { return hitPoints; }
-
-    public Brick create(double x, double y) {
-        return switch (this) {
-            case NORMAL -> new NormalBrick(x, y);
-            case STRONG -> new StrongBrick(x, y);
-            case WIDE -> new Brick(x, y, width, height, hitPoints);
-            default -> new Brick(x, y, width, height, hitPoints);
-        };
-    }
+    NORMAL,
+    STRONG,
+    UNBREAKABLE,
+    POWERUP_GUARANTEED,
+    EXPLOSIVE
 }
