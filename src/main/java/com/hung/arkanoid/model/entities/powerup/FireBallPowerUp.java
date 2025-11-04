fc9dfc80550dad7b859f0b3f6bc185eaa0c05ca7
viethung <viethung12062005@gmail.com>
2025-11-04 23:49:08 +0700

    Bổ sung thêm comment

--- src/main/java/com/hung/arkanoid/model/entities/powerup/FireBallPowerUp.java
+++ src/main/java/com/hung/arkanoid/model/entities/powerup/FireBallPowerUp.java
@@ -2,8 +2,12 @@ package com.hung.arkanoid.model.entities.powerup;
 
 import com.hung.arkanoid.game.GameManager;
 
+/**
+ * Power-up that turns all balls into fireballs, allowing them to pass
+ * through bricks for a limited duration.
+ */
 public class FireBallPowerUp extends PowerUp {
-    private static final double DURATION = 8.0;
+    private static final double DURATION = 8.0; // seconds
 
     public FireBallPowerUp(double x, double y) {
         super(x, y, PowerUpType.FIRE_BALL);
